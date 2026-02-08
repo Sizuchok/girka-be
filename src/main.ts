ef9ed2e11c3618862e498c89a87765eeebc4335c
Illia Sizhuk <isizhuk@s-pro.io>
2026-02-08 20:26:31 +0200

    chore: add Scalar for OpenAPI UI

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { cleanupOpenApiDoc } from 'nestjs-zod';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
     .setTitle('Girka')
     .setDescription('The Girka API description.')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const documentFactory = () => cleanupOpenApiDoc(SwaggerModule.createDocument(app, config));
@@ -17,6 +19,14 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, documentFactory, {
     jsonDocumentUrl: 'swagger/json',
   });
+
+  app.use(
+    '/reference',
+    apiReference({
+      content: documentFactory(),
+      theme: 'deepSpace',
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
